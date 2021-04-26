3134acf6ca1d52f6c93091e60e1c6438e7c325c8
Fadeev2000 <fda2000@ua.fm>
2021-04-26 22:58:17 +0300

    Добавил все по заданию Шаблонизация

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-import './sass/main.scss';
+import menu from './menu.json';
+import itemsTemplate from './menu-items.hbs';
+
+
+//import './sass/main.scss';
+console.log(itemsTemplate);
+
+const menuRef = document.querySelector('.js-menu');
+const markup = itemsTemplate(menu);
+
+console.log(markup);
+
+menuRef.insertAdjacentHTML('beforeend', markup);
