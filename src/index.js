bf131f7a31105ba5ad95e9dc52b3a87285fe8c67
Fadeev2000 <fda2000@ua.fm>
2021-05-12 12:54:02 +0300

    Добавлена реализация переключателя через класс

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import menu from './menu.json';
 import itemsTemplate from './templates/menu-items.hbs';
-
-
 //import './sass/main.scss';
-//console.log(itemsTemplate);
+
 const Theme = {
   LIGHT: 'light-theme',
   DARK: 'dark-theme',
@@ -13,58 +11,56 @@ const LOCALSTORAGE_KYE = 'theme';
 const menuRef = document.querySelector('.js-menu');
 const markup = itemsTemplate(menu);
 
-//console.log(markup);
-
 menuRef.insertAdjacentHTML('beforeend', markup);
 
-const bodyEl = document.querySelector('body');
-const switchEl = document.querySelector('.theme-switch__toggle');
-switchEl.addEventListener('change', onChange);
+const refs = {
+    bodyEl: document.querySelector('body'),
+    switchEl: document.querySelector('.theme-switch__toggle')
+};
 
-
-if (localStorage.getItem('theme') !== null) {
-    const selectedTheme = localStorage.getItem(LOCALSTORAGE_KYE);
-    //console.log('selectedTheme->' + selectedTheme);
-    //console.log(Theme.LIGHT);
-    if (selectedTheme === Theme.LIGHT) {
-        //console.log('Light-> ' + localStorage.getItem('theme'));
-        bodyEl.classList.add(Theme.LIGHT);
-        switchEl.checked = false;
-    } else {
-        //console.log('dark-> ' + localStorage.getItem('theme'));
-        bodyEl.classList.add(Theme.DARK);
-        switchEl.checked = true;
+class Switch {
+    constructor(refs) {
+        this.bodyEl = refs.bodyEl;
+        this.switchEl = refs.switchEl;
     }
+
+
+    switch() {
+        if (localStorage.getItem(LOCALSTORAGE_KYE) !== null) {
+            const selectedTheme = localStorage.getItem(LOCALSTORAGE_KYE);
+    
+            if (selectedTheme === Theme.LIGHT) {
+                this.bodyEl.classList.add(Theme.LIGHT);
+                this.switchEl.checked = false;
+            } else {
+                this.bodyEl.classList.add(Theme.DARK);
+                this.switchEl.checked = true;
+            }
+        }
+    }
+
 }
+
+const switcherTheme = new Switch(refs);
+switcherTheme.switch();
      
-
-
 function onChange() {
-    //console.log(switchEl.checked);
-    //console.log(localStorage.getItem('theme'));
-
     switch (localStorage.getItem(LOCALSTORAGE_KYE)) {
         case Theme.LIGHT:
             localStorage.setItem(LOCALSTORAGE_KYE, Theme.DARK);
-            bodyEl.classList.replace(Theme.LIGHT, Theme.DARK);
-            switchEl.checked = true;
+            refs.bodyEl.classList.replace(Theme.LIGHT, Theme.DARK);
+            refs.switchEl.checked = true;
             break;
         case Theme.DARK:
             localStorage.setItem(LOCALSTORAGE_KYE, Theme.LIGHT);
-            bodyEl.classList.replace(Theme.DARK, Theme.LIGHT);
-            switchEl.checked = false;
+            refs.bodyEl.classList.replace(Theme.DARK, Theme.LIGHT);
+            refs.switchEl.checked = false;
             break;
         default: //=== null
             localStorage.setItem(LOCALSTORAGE_KYE, Theme.DARK);
-            bodyEl.classList.add(Theme.DARK);
-            switchEl.checked = true;
+            refs.bodyEl.classList.add(Theme.DARK);
+            refs.switchEl.checked = true;
     }
-
-    
-    //localStorage.setItem('theme', Theme.DARK)
-
-
-
-    //bodyEl.classList.add('.light-theme');
-    //bodyEl.classList.add('.dark-theme');
 }
+
+refs.switchEl.addEventListener('change', onChange);
